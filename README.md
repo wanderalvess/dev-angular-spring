e1a117232547b81f4446099017cf7e9d83da0bb8
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2022-11-03 15:40:14 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+"# dev-angular-spring" 
+
+Criar projeto: ng new name-project   
+
+Site material exmeplos: [material](https://material.angular.io)   
+
+Gerar modulo: ng g m dev --routing   
+
+Gerar componente: ng g c dev/dev
+
+
---
+++ src/app/dev/dev.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DevRoutingModule } from './dev-routing.module';
+import { DevComponent } from './dev/dev.component';
+
+
+@NgModule({
+  declarations: [
+    DevComponent
+  ],
+  imports: [
+    CommonModule,
+    DevRoutingModule
+  ]
+})
+export class DevModule { }
